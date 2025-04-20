2b4a00ab8a146865ce2e544417f2a4ed121fc773
matheusbarros <matheus53barros@hotmail.com>
2025-04-20 10:52:21 -0300

    chore: add option to chose default text editor

--- install.sh
+++ install.sh
@@ -8,17 +8,19 @@ update_system(){
 
 install_default_text_editor(){
     PS3='Please enter your default text editor: '
-    options=("Install Vscode" "Install Vim")
+    options=("Install vscode" "Install vim")
     select opt in "${options[@]}"
     do
         case $opt in
-            "Install Vscode")
+            "Install vscode")
                 echo "You chose to install VSCode"
                 install_vscode
+                break
                 ;;
-            "Install Vim")
+            "Install vim")
                 echo "You chose to install Vim"
                 install_vim
+                break
                 ;;
             *)
                 echo "Invalid option: $REPLY"
@@ -64,13 +66,13 @@ install_curl() {
 install_oh_my_zsh() {
   echo "Installing Oh My Zsh..."
   yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-  echo "Zsh installed succesfully"
+  echo "Zsh installed successfully"
 }
 
 install_tmux() {
   echo "Installing Tmux..."
   sudo apt install -y tmux
-  echo "Tmux installed succesfully"
+  echo "Tmux installed successfully"
 }
 
 install_tpm(){
@@ -82,7 +84,7 @@ install_tpm(){
   fi
 
   cp tmux/.tmux.conf ~/.tmux.conf
-  echo "Tmux installed succesfully"
+  echo "Tmux installed successfully"
   echo "Open a new tmux session and press 'prefix + I' to install all plugins"
 }
 
@@ -91,7 +93,7 @@ install_fonts() {
   sudo mkdir -p ~/.fonts
   sudo cp -a ./fonts/. ~/.fonts
   sudo fc-cache -fv
-  echo "Fonts installed"
+  echo "Fonts installed successfully"
 }
 
 install_spaceship_theme() {
@@ -102,13 +104,44 @@ install_spaceship_theme() {
   sudo cp ./zsh/spaceship.zsh ~/.config
   sudo cp ./zsh/.zshrc ~/.zshrc
   source ~/.zshrc
-  echo "Spaceship theme installed succesfully"
+  echo "Spaceship theme installed successfully"
 }
 
 install_catppuccin_theme() {
   echo "Installing Catppuccin theme..."
   curl -L https://raw.githubusercontent.com/catppuccin/gnome-terminal/v1.0.0/install.py | python3 -
-  echo "Catppuccin theme installed succesfully"
+  echo "Catppuccin theme installed successfully"
+}
+
+install_docker(){
+  echo "Installing Docker..."
+
+  # Add Docker's official GPG key:
+  sudo apt-get update
+  sudo apt-get install ca-certificates curl
+  sudo install -m 0755 -d /etc/apt/keyrings
+  sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+  sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+  # Add the repository to Apt sources:
+  echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+    $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo apt-get update
+
+  # Install the latest Docker packages
+  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+  # Create the docker user group
+  sudo groupadd docker
+  sudo usermod -aG docker $USER
+
+  # Configure Docker to start on boot with systemd
+  sudo systemctl enable docker.service
+  sudo systemctl enable containerd.service
+
+  echo "Docker installed successfully"
 }
 
 if [ -d "$HOME/dotfiles" ]
@@ -127,4 +160,5 @@ else
   install_fonts
   install_spaceship_theme
   install_catppuccin_theme
+  echo "Dotfile installed successfully"
 fi
